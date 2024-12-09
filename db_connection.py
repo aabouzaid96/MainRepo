3993b050913906c5f5a049134eefe26fd9f91871
abouzaid <a.abouzaid@aems.ai>
2024-12-09 23:39:40 +0200

    add logging

--- db_connection.py
+++ db_connection.py
@@ -4,6 +4,11 @@ from PIPELINE_POC.settings import DATABASES
 def get_connection():
     try:
         db_config = DATABASES['default']
+
+        print('db_config[HOST]', db_config['HOST'])
+        print('db_config[PORT]', db_config['PORT'])
+        print('db_config[USER]', db_config['USER'])
+        print('db_config[NAME]', db_config['NAME'])
         connection = psycopg2.connect(
             host=db_config['HOST'],
             port=db_config['PORT'],
