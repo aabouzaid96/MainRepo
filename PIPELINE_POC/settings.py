52d2a7db0579952ef9471dddb6fae1e62f1d7f3e
abouzaid <a.abouzaid@aems.ai>
2024-12-12 12:20:19 +0200

    working deployment

--- PIPELINE_POC/settings.py
+++ PIPELINE_POC/settings.py
@@ -49,11 +49,11 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME', default='pipeline_poc'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgresAWS0'),
+        'HOST': config('DB_HOST', default='172.31.37.63'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
